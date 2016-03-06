cf2746aea91f420fec13c854c5e42cac01383162
Casey Ward <caseyward1@gmail.com>
2016-03-06 12:23:26 -0800

    merging newly created tableviewcell subclass

---
+++ Tallyman/tmTableViewCell.m
@@ -0,0 +1,13 @@
+//
+//  tmTableViewCell.m
+//  Tallyman
+//
+//  Created by Casey Ward on 3/6/16.
+//  Copyright © 2016 Casey Wrd. All rights reserved.
+//
+
+#import "tmTableViewCell.h"
+
+@implementation tmTableViewCell
+
+@end
