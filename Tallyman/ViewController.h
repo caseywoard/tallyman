3109b2a21b439a954aaf631b77fa4ba20a5727c1
Casey Ward <caseyward1@gmail.com>
2016-03-05 18:40:11 -0800

    very first commit

---
+++ Tallyman/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tallyman
+//
+//  Created by Casey Ward on 3/5/16.
+//  Copyright © 2016 Casey Wrd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Tallyman/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Tallyman
+//
+//  Created by Casey Ward on 3/5/16.
+//  Copyright © 2016 Casey Wrd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
